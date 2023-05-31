39d84c6a1b327fd711d5b81639ce22745b32fcdd
beatazalewa <beatazalewa@noreply.github.com>
2023-05-31 13:08:21 +0000

    Added migration

--- api_events/admin.py
+++ api_events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Appointment)
+admin.site.register(AppointmentReason)
+admin.site.register(AppointmentStatus)
+admin.site.register(AppointmentType)
