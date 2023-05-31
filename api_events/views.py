2b2bddafa7879f8dfc398b031c64664bd3c61845
beatazalewa <beatazalewa@noreply.github.com>
2023-05-31 13:40:48 +0000

    Added new view

--- api_events/views.py
+++ api_events/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("This is our homepage")
-
-def events(request):
-    return HttpResponse("This is our events page")
+class AppointmentView(generics.CreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
